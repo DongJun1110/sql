c42fa59fbb2f59e29964559c0f93827aaac395b7
Kimdongjun <r2222911r@ajou.ac.kr>
2024-07-12 15:00:48 +0900

    멸종 위기의 대장균 찾기

---
+++ 멸종위기의대장균찾기.sql
@@ -0,0 +1,15 @@
+WITH RECURSIVE CTE AS (SELECT ID, PARENT_ID, 1 AS GENERATION 
+                       FROM ECOLI_DATA 
+                       WHERE PARENT_ID IS NULL
+                      
+                       UNION ALL
+                       
+                       SELECT ED.ID, ED.PARENT_ID, C.GENERATION+1
+                       FROM CTE C INNER JOIN ECOLI_DATA ED ON C.ID = ED.PARENT_ID
+                      )
+
+SELECT COUNT(*) AS COUNT, GENERATION
+FROM CTE 
+WHERE ID NOT IN (SELECT DISTINCT PARENT_ID FROM CTE WHERE PARENT_ID IS NOT NULL)
+GROUP BY GENERATION
+ORDER BY 2
